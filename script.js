ca8117f3b8b30775f8447ed9ac4c43de06ad630d
dinaamlll <dinamaliaa17@gmail.com>
2024-06-01 20:15:34 +0700

    Mengambil Nilai dari Formulir

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 document.getElementById("survey-form").addEventListener("submit", function(event){
     event.preventDefault(); 
-
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var dob = document.getElementById("dob").value;
+    var favoriteGenre = document.getElementById("favoriteGenre").value;
+    var opinion = document.querySelector('input[name="opinion"]:checked').value;
+    var comment = document.getElementById("comment").value;
+    
 });
